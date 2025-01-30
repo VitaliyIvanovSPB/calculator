a51cd67e86a7fc57375e66e7485d743a0e5e99a1
VitaliyIvanovSPB <v63@list.ru>
2025-01-31 01:43:03 +0300

    calculation works, except vsan and total RUB

--- data.py
+++ data.py
@@ -1,8 +1,133 @@
-
-cpus_intel = [
+cpus = [
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 7343 (16x3.2 GHz)',
+        'cores_quantity': 16,
+        'cores_frequency': 3200,
+        'price': 1420,
+        'socket': 4094,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 7513 (32x2.6 GHz)',
+        'cores_quantity': 32,
+        'cores_frequency': 2600,
+        'price': 1200,
+        'socket': 4094,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 7763 (64x2.45 GHz)',
+        'cores_quantity': 64,
+        'cores_frequency': 2450,
+        'price': 2515,
+        'socket': 4094,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 9354 (32x3.25 GHz)',
+        'cores_quantity': 32,
+        'cores_frequency': 3250,
+        'price': 2400,
+        'socket': 6096,
+        'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 9754 SP5 (128x2.25GHz)',
+        'cores_quantity': 128,
+        'cores_frequency': 2250,
+        'price': 4200,
+        'socket': 6096,
+        'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 9474F (48x3.6 GHz)',
+        'cores_quantity': 48,
+        'cores_frequency': 3600,
+        'price': 4495,
+        'socket': 6096,
+        'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 9654 (96x2.4 GHz)',
+        'cores_quantity': 96,
+        'cores_frequency': 2400,
+        'price': 3100,
+        'socket': 6096,
+        'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'amd',
+        'name': 'EPYC 9274F (24x4.05 GHz)',
+        'cores_quantity': 24,
+        'cores_frequency': 4050,
+        'price': 3200,
+        'socket': 6096,
+        'ram_gen': 'DDR5',
+    },
     {
         'manufacturer': 'intel',
-        'name': 'Gold 6336Y',
+        'name': 'Gold 6240 (18x2.6 GHz HT)',
+        'cores_quantity': 18,
+        'cores_frequency': 2600,
+        'price': 1500,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Gold 6240R (24x2.4 GHz HT)',
+        'cores_quantity': 24,
+        'cores_frequency': 2400,
+        'price': 2000,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Gold 6242R (20x3.1 GHz HT)',
+        'cores_quantity': 20,
+        'cores_frequency': 3100,
+        'price': 1840,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Gold 6248R (24x3.0 GHz HT)',
+        'cores_quantity': 24,
+        'cores_frequency': 3000,
+        'price': 2823,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Gold 6254 (18x3.1 GHz HT)',
+        'cores_quantity': 18,
+        'cores_frequency': 3100,
+        'price': 3000,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Gold 6256 (12x3.6 GHz HT)',
+        'cores_quantity': 12,
+        'cores_frequency': 3600,
+        'price': 4200,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Gold 6336Y (24x2.4 GHz HT)',
         'cores_quantity': 24,
         'cores_frequency': 2400,
         'price': 2174,
@@ -11,16 +136,16 @@ cpus_intel = [
     },
     {
         'manufacturer': 'intel',
-        'name': 'Silver 4314',
-        'cores_quantity': 16,
-        'cores_frequency': 2400,
-        'price': 784,
+        'name': 'Gold 6354 (18x3.0 GHz HT)',
+        'cores_quantity': 18,
+        'cores_frequency': 3000,
+        'price': 2500,
         'socket': 4189,
         'ram_gen': 'DDR4',
     },
     {
         'manufacturer': 'intel',
-        'name': 'Gold 6444Y',
+        'name': 'Gold 6444Y (16x3.6 GHz)',
         'cores_quantity': 16,
         'cores_frequency': 3600,
         'price': 3376,
@@ -29,122 +154,97 @@ cpus_intel = [
     },
     {
         'manufacturer': 'intel',
-        'name': 'Silver 4516Y+',
-        'cores_quantity': 24,
-        'cores_frequency': 2200,
-        'price': 1777,
+        'name': 'Gold 6544Y (16x3.6 GHz)',
+        'cores_quantity': 16,
+        'cores_frequency': 3600,
+        'price': 5439,
         'socket': 4677,
         'ram_gen': 'DDR5',
     },
     {
         'manufacturer': 'intel',
-        'name': 'Gold 6544',
+        'name': 'Xeon Gold 6526Y (16x2.8 GHz)',
         'cores_quantity': 16,
-        'cores_frequency': 3650,
-        'price': 5439,
+        'cores_frequency': 2800,
+        'price': 1800,
         'socket': 4677,
-        'ram_gen': 'ddr5',
-    },
-]
-cpus_amd = [
-    {
-        'manufacturer': 'amd',
-        'name': 'EPYC 7343',
-        'cores_quantity': 16,
-        'cores_frequency': 3200,
-        'price': 1420,
-        'soket': 4094,
-        'ram_gen': 'DDR4',
-    },
-    {
-        'manufacturer': 'amd',
-        'name': 'EPYC 7513',
-        'cores_quantity': 32,
-        'cores_frequency': 2600,
-        'price': 1200,
-        'soket': 4094,
-        'ram_gen': 'DDR4',
-    },
-    {
-        'manufacturer': 'amd',
-        'name': 'EPYC 7763',
-        'cores_quantity': 64,
-        'cores_frequency': 2450,
-        'price': 2515,
-        'soket': 4094,
-        'ram_gen': 'DDR4',
-    },
-    {
-        'manufacturer': 'amd',
-        'name': 'EPYC 9354',
-        'cores_quantity': 32,
-        'cores_frequency': 3250,
-        'price': 2400,
-        'soket': 6096,
-        'ram_gen': 'DDR4',
-    },
-    {
-        'manufacturer': 'amd',
-        'name': 'EPYC 9274',
-        'cores_quantity': 24,
-        'cores_frequency': 4050,
-        'price': 3200,
-        'soket': 6096,
         'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Xeon Gold 6530 (32x2.1 GHz)',
+        'cores_quantity': 32,
+        'cores_frequency': 2100,
+        'price': 1850,
+        'socket': 4677,
+        'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'PLATINUM 8276 (28x2.2 GHz)',
+        'cores_quantity': 28,
+        'cores_frequency': 2200,
+        'price': 7427,
+        'socket': 3647,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'PLATINUM 8360H (24x3.0 GHz)',
+        'cores_quantity': 24,
+        'cores_frequency': 3000,
+        'price': 3214,
+        'socket': 4189,
+        'ram_gen': 'DDR5',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Silver 4314 (16x2.4 GHz HT)',
+        'cores_quantity': 16,
+        'cores_frequency': 2400,
+        'price': 784,
+        'socket': 4189,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Silver 4510 (12x2.4 GHz HT)',
+        'cores_quantity': 12,
+        'cores_frequency': 2400,
+        'price': 863,
+        'socket': 4677,
+        'ram_gen': 'DDR4',
+    },
+    {
+        'manufacturer': 'intel',
+        'name': 'Silver 4516Y+ (24x2.2 GHz HT)',
+        'cores_quantity': 24,
+        'cores_frequency': 2200,
+        'price': 1777,
+        'socket': 4677,
+        'ram_gen': 'DDR4',
     },
 ]
 
 servers = [
     {
-        'name': 'ASUS RS720 - E11 - RS12U',
+        'name': 'ASUS ESC8000-G4',
         'cpu_vendor': 'intel',
-        'socket': 4677,
-        'max_ram': 32,
-        'ram_gen': 'ddr5',
-        'price': 5000,
+        'socket': 3647,
+        'max_ram': 24,
+        'ram_gen': 'ddr4',
+        'price': 7409,
     },
     {
-        'name': 'ASUS RS700 - E11 - RS12U',
-        'cpu_vendor': 'intel',
-        'socket': 4677,
-        'max_ram': 32,
-        'ram_gen': 'ddr5',
-        'price': 4400,
-    },
-    {
-        'name': 'ASUS RS720A - E11 - RS12',
+        'name': 'ASUS ESC8000A-E11',
         'cpu_vendor': 'amd',
         'socket': 4094,
         'max_ram': 32,
         'ram_gen': 'ddr4',
-        'price': 6000,
+        'price': 7100,
     },
-    {
-        'name': 'ASUS RS720A - E11 - RS24U',
-        'cpu_vendor': 'amd',
-        'socket': 4094,
-        'max_ram': 32,
-        'ram_gen': 'ddr4',
-        'price': 7000,
-    },
-    {
-        'name': 'ASUS RS720A - E12 - RS12',
-        'cpu_vendor': 'amd',
-        'socket': 6096,
-        'max_ram': 24,
-        'ram_gen': 'ddr5',
-        'price': 6259,
-    },
-    {
-        'name': 'ASUS RS720A - E12 - RS24',
-        'cpu_vendor': 'amd',
-        'socket': 6096,
-        'max_ram': 24,
-        'ram_gen': 'ddr5',
-        'price': 7200,
-    },
-    {
-        'name': 'ASUS RS700 - E10 - RS12U',
+{
+        'name': 'ASUS RS700-E10-RS12U',
         'cpu_vendor': 'intel',
         'socket': 4189,
         'max_ram': 32,
@@ -152,14 +252,189 @@ servers = [
         'price': 4000,
     },
     {
-        'name': 'Gigabyte R282',
+        'name': 'ASUS RS700-E11-RS12U',
+        'cpu_vendor': 'intel',
+        'socket': 4677,
+        'max_ram': 32,
+        'ram_gen': 'ddr5',
+        'price': 4410,
+    },
+    {
+        'name': 'ASUS RS700A-E11-RS4U 1U',
+        'cpu_vendor': 'amd',
+        'socket': 4094,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 2370,
+    },
+    {
+        'name': 'ASUS RS700A-E12-RS12U',
+        'cpu_vendor': 'amd',
+        'socket': 6096,
+        'max_ram': 24,
+        'ram_gen': 'ddr5',
+        'price': 5500,
+    },
+    {
+        'name': 'ASUS RS720-E11-RS24',
+        'cpu_vendor': 'intel',
+        'socket': 4677,
+        'max_ram': 32,
+        'ram_gen': 'ddr5',
+        'price': 6525,
+    },
+    {
+        'name': 'ASUS RS720-E10-RS24U 2U',
+        'cpu_vendor': 'intel',
+        'socket': 4189,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 6846,
+    },
+    {
+        'name': 'ASUS RS720-E11-RS12U',
+        'cpu_vendor': 'intel',
+        'socket': 4677,
+        'max_ram': 32,
+        'ram_gen': 'ddr5',
+        'price': 6425,
+    },
+    {
+        'name': 'ASUS RS720A-E11-RS12 2U',
+        'cpu_vendor': 'amd',
+        'socket': 4094,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 6000,
+    },
+    {
+        'name': 'ASUS RS720A-E11-RS24U',
+        'cpu_vendor': 'amd',
+        'socket': 4094,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 7000,
+    },
+    {
+        'name': 'ASUS RS720A-E12-RS12',
+        'cpu_vendor': 'amd',
+        'socket': 6096,
+        'max_ram': 24,
+        'ram_gen': 'ddr5',
+        'price': 6259,
+    },
+    {
+        'name': 'ASUS RS720A-E12-RS24U',
+        'cpu_vendor': 'amd',
+        'socket': 6096,
+        'max_ram': 24,
+        'ram_gen': 'ddr5',
+        'price': 8285,
+    },
+    {
+        'name': 'Gigabyte R282-3C0 2U',
         'cpu_vendor': 'intel',
         'socket': 4189,
         'max_ram': 32,
         'ram_gen': 'ddr4',
         'price': 2490,
     },
-
+    {
+        'name': 'Gigabyte R292-4S0',
+        'cpu_vendor': 'intel',
+        'socket': 4189,
+        'max_ram': 48,
+        'ram_gen': 'ddr4',
+        'price': 6850,
+    },
+    {
+        'name': 'Inspur NF5280A6',
+        'cpu_vendor': 'amd',
+        'socket': 4094,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 5347,
+    },
+    {
+        'name': 'Inspur NF5280M5',
+        'cpu_vendor': 'intel',
+        'socket': 3647,
+        'max_ram': 24,
+        'ram_gen': 'ddr4',
+        'price': 5200,
+    },
+    {
+        'name': 'Inspur NF5280M6',
+        'cpu_vendor': 'intel',
+        'socket': 4189,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 6000,
+    },
+    {
+        'name': 'INTEL M50CYP1UR212',
+        'cpu_vendor': 'intel',
+        'socket': 4189,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 2320,
+    },
+    {
+        'name': 'INTEL M50CYP2UR312',
+        'cpu_vendor': 'intel',
+        'socket': 4189,
+        'max_ram': 32,
+        'ram_gen': 'ddr4',
+        'price': 2890,
+    },
+    {
+        'name': 'INTEL R1208WFTZSR',
+        'cpu_vendor': 'intel',
+        'socket': 3647,
+        'max_ram': 24,
+        'ram_gen': 'ddr4',
+        'price': 3805,
+    },
+    {
+        'name': 'INTEL R2312WFTZSR',
+        'cpu_vendor': 'intel',
+        'socket': 3647,
+        'max_ram': 24,
+        'ram_gen': 'ddr4',
+        'price': 1605,
+    },
+    {
+        'name': 'SUPERMICRO SYS-121H-TNR',
+        'cpu_vendor': 'intel',
+        'socket': 4677,
+        'max_ram': 32,
+        'ram_gen': 'ddr5',
+        'price': 8500,
+    },
+    {
+        'name': 'SUPERMICRO AS-1125HS-TNR',
+        'cpu_vendor': 'amd',
+        'socket': 6096,
+        'max_ram': 24,
+        'ram_gen': 'ddr5',
+        'price': 7000,
+    },
+    {
+        'name': 'SUPERMICRO SYS-4029GP-TRT',
+        'cpu_vendor': 'intel',
+        'socket': 3647,
+        'max_ram': 24,
+        'ram_gen': 'ddr4',
+        'price': 7500,
+    },
+    {
+        'name': 'SUPERMICRO SYS-221H-TN24R',
+        'cpu_vendor': 'intel',
+        'socket': 4677,
+        'max_ram': 32,
+        'ram_gen': 'ddr5',
+        'price': 12000,
+    },
 ]
 
 rams = [
@@ -196,20 +471,10 @@ rams = [
 
 ]
 
-esxi_disc = [
-    {
-        'type': 'sata',
-        'size': '480',
-        'vendor': 'micron 5300',
-        'price': '100'
-    },
-    {
-        'type': 'm2',
-        'size': '512',
-        'vendor': 'intel 670p',
-        'price': '100'
-    },
-]
+esxi_disc = {
+    'type': '512Gb sata/m2',
+    'price': 100
+}
 
 cache_disc = [
     {
@@ -218,6 +483,23 @@ cache_disc = [
         'vendor': 'micron 7450',
         'price': '300'
     },
+]
+
+network_card = {
+    'name': '25GB DUAL MCX512A',
+    'price': 270,
+}
+
+hba_adapter = [
+    {
+        'name': 'LSI9300-8i',
+        'spec': 8,
+        'price': '$330',
+    }, {
+        'name': 'LSI9560-16i',
+        'spec': 16,
+        'price': '$1100',
+    }
 ]
 
 #
@@ -235,18 +517,6 @@ cache_disc = [
 # ssdnvme: 7.68TbMicron7400Pro    $1 500    $0, 195 7680
 # ssdnvme: 15.36TbNVMeEnterpriseSamsung    $2 600    $0, 169 15360
 #
-# СетеваякартаPCI: СтоимостьСтоимость1ед.Тех.показатели
-# Нет    $0
-# 2x10GBDUALMCX4121A - XCAT    $110    $110 10G, SFP28
-# 2 × 25GEDUALMCX4121A - ACAT    $170    $170 10 / 25G, SFP28
-# 2 × 25GBDUALMCX512A - ACAT    $270    $270 10 / 25G, SFP28
-# 2 × 25GBDUALMCX512F - ACAT    $270    $270 25G, SFP28
-#
-# HBA
-# PCI: СтоимостьСтоимость1ед.Тех.показатели
-# Нет
-# hbacontroller: LSI9300 -8i    $330    $330 SAS / SATA HBAадаптер
-# hbacontroller: LSI9560 - 16i    $1100    $1100 NVMe / SAS / SATARAIDконтроллер
 #
 # GPUСтоимостьСтоимость1ед.Тех.показателиCUDAядра
 # Нет    $0
--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 import math
-from data import cpus_intel, cpus_amd, rams, servers
+from data import cpus, rams, servers, esxi_disc, network_card
+
+
+def get_cpus(vendor: str, min_frequency: int):
+    """Возвращает список процессоров нужного вендора, отфильтрованных по минимальной частоте"""
+    if vendor == 'any':
+        return [cpu for cpu in cpus if cpu['cores_frequency'] >= min_frequency]
+    else:
+        return [cpu for cpu in cpus if cpu['manufacturer'] == vendor and cpu['cores_frequency'] >= min_frequency]
 
 
 def requested_config(cpu_vendor: str = 'any', vcpu: int = 400, vram: int = 800, vssd: int = 20000,
                      cpu_overcommit: int = 3, cpu_min_frequency: int = 0, max_cpu_usage: float = 0.8,
-                     max_ram_usage: float = 0.8):
+                     max_ram_usage: float = 0.8, currency: int = 100):
+    """Подбирает конфигурации серверов по заданным параметрам"""
+
     all_configs = {}
-    match cpu_vendor:
-        case 'any':
-            for cpu in cpus_intel:
-                vcpu_on_host = cpu['cores_quantity'] * 2 * max_cpu_usage
-                cpu_hosts = math.ceil(vcpu / cpu_overcommit / vcpu_on_host)
-                print(
-                    f"CPU name = {cpu['name']}, need host by cpu = {cpu_hosts}, total price = {cpu_hosts * cpu['price']} $")
-            for cpu in cpus_amd:
-                vcpu_on_host = cpu['cores_quantity'] * 2 * max_cpu_usage
-                cpu_hosts = math.ceil(vcpu / cpu_overcommit / vcpu_on_host)
-                print(
-                    f"CPU name = {cpu['name']}, need host by cpu = {cpu_hosts}, total price = {cpu_hosts * cpu['price']} $")
-        case 'intel':
-            for cpu in cpus_intel:
-                if cpu_min_frequency < cpu['cores_frequency']:
-                    cpu_hosts = math.ceil(vcpu / cpu_overcommit / (cpu['cores_quantity'] * 2 * max_cpu_usage))
-                    for server in servers:
-                        if server['socket'] == cpu['socket']:
-                            for ram in rams:
-                                if server['ram_gen'] == ram['ram_gen']:
-                                    ram_host = math.ceil(vram / max_ram_usage / cpu_hosts / ram['ram_size'])
-                                    ram_1host = ram_host + 1 if ram_host % 2 == 1 else ram_host
-                                    if ram_1host > server['max_ram']:
-                                        continue
-                                    all_configs[len(all_configs)] = {'Need hosts by CPU:': cpu_hosts,
-                                                                     'CPU:': cpu['name'],
-                                                                     'Server: ': server['name'],
-                                                                     ram['ram_size']: ram_1host}
-                        else:
-                            continue
-                else:
+    cpu_list = get_cpus(cpu_vendor, cpu_min_frequency)
+
+    for cpu in cpu_list:
+        cpu_hosts = math.ceil(vcpu / cpu_overcommit / (cpu['cores_quantity'] * 2 * max_cpu_usage))
+
+        for server in filter(lambda s: s['socket'] == cpu['socket'], servers):
+            for ram in filter(lambda r: r['ram_gen'] == server['ram_gen'], rams):
+                ram_host = math.ceil(vram / max_ram_usage / cpu_hosts / ram['ram_size'])
+                ram_1host = ram_host + 1 if ram_host % 2 == 1 else ram_host
+
+                if ram_1host > server['max_ram']:
                     continue
-            return all_configs
-        case 'amd':
-            for cpu in cpus_amd:
-                vcpu_on_host = cpu['cores_quantity'] * 2 * max_cpu_usage
-                cpu_hosts = math.ceil(vcpu / cpu_overcommit / vcpu_on_host)
-                print(
-                    f"CPU name = {cpu['name']}, need host by cpu = {cpu_hosts}, total price = {cpu_hosts * cpu['price']} $")
-        case _:
-            return 'хз кто это!'
+                total_price = (cpu['price'] + server['price'] + ram_1host * ram['price'] + esxi_disc['price'] +
+                               network_card['price']) * (cpu_hosts+1)
+
+                all_configs[len(all_configs)] = {'Need hosts by CPU': cpu_hosts+1,
+                                                 'CPU': cpu['name'] + ' ' + str(cpu['cores_frequency']) + 'GHz',
+                                                 'Server': server['name'],
+                                                 ram['ram_size']: ram_1host,
+                                                 'Esxi disk': esxi_disc['type'],
+                                                 'Network card': network_card['name'],
+                                                 'Total price': total_price}
+    sorted_configs = sorted(all_configs.items(), key=lambda x: x[1]['Total price'])[:5]
+    cheapest_config = min(all_configs.items(), key=lambda x: x[1]['Total price'])
+    for index, (config_id, config) in enumerate(sorted_configs, 1):
+        conf = [f"\nТоп {index}: Вариант {config_id}\n"]
+        for key, value in config.items():
+            conf.append(f"{key}: {value}\n")
+        print(''.join(conf))
+    return cheapest_config
 
 
 if __name__ == '__main__':
-    print(requested_config(vcpu=4000, vram=16000, vssd=100000, cpu_vendor='intel', cpu_min_frequency=3600))
+    print(requested_config(vcpu=2800, vram=10600, vssd=15000))
 
 # TODO:
 # вся логика пока только на интеле
