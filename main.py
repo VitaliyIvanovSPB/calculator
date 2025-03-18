30295596b76b5c877beb0f283333490309c1d7df
VitaliyIvanovSPB <v63@list.ru>
2025-03-18 17:42:10 +0300

    data

--- main.py
+++ main.py
@@ -31,14 +31,14 @@ class MessageRequest(BaseModel):
 
 @app.post('/calculate')
 def calculate(request: MessageRequest):
-    data = json.loads(request.data)
-    params = [f'{k}={v}' for k, v in data.items() if v]
-    requirements = ' '.join(params)
-    user = request.user_id
-    text = 'Copy command bellow and send to me\n'
-    command_calc = f'/calculate {requirements}'
-    send_message(user, text)
-    send_message(user, command_calc)
+    print(request)
+    # params = [f'{k}={v}' for k, v in data.items() if v]
+    # requirements = ' '.join(params)
+    # user = request.user_id
+    # text = 'Copy command bellow and send to me\n'
+    # command_calc = f'/calculate {requirements}'
+    # send_message(user, text)
+    # send_message(user, command_calc)
 
 
 async def run_fastapi():
