d8169540fd35f171e5e0c88ec8599d9bfa598e1e
vitaliyivanovspb <v63@list.ru>
2025-06-05 13:45:15 +0300

    min 4 hosts

--- bot/calculator.py
+++ bot/calculator.py
@@ -61,69 +61,70 @@ def create_config(all_configs, capacity_disk_type, cpu, cpu_hosts, cpu_overcommi
     raid_config = get_raid_config()
     n = n_qty(cpu_hosts)
     cpu_hosts_n = cpu_hosts + n
-    key = 6 if cpu_hosts_n >= 6 else (5 if cpu_hosts_n == 5 else 1)
-    vsan_raw = vssd * raid_config[key]['disk_usage_overhead'] / (1 - slack_space)
+    if cpu_hosts_n > 3:
+        key = 6 if cpu_hosts_n >= 6 else (5 if cpu_hosts_n == 5 else 1)
+        vsan_raw = vssd * raid_config[key]['disk_usage_overhead'] / (1 - slack_space)
 
-    for disk_size, disk_groups in get_raids_data()[key][slack_space].items():
-        for disk_group, disks_capacity in disk_groups.items():
-            host_disks_qty = int(str(disk_group)[0]) * int(str(disk_group)[1]) + int(str(disk_group)[0]) + 1 if capacity_disk_type == 'ssd' else 0
-            host_disks_hba_qty = int(str(disk_group)[0]) * int(str(disk_group)[1])
-            vsan_and_disks_limit = check_vsan_and_disks_limit(cpu_hosts, disks_capacity, host_disks_qty, server,
-                                                              vsan_raw, vssd)
-            if vsan_and_disks_limit:
-                if capacity_disk_type == 'ssd':
-                    hba = hba_adapter[8] if host_disks_hba_qty < 9 else hba_adapter[16]
-                else:
-                    hba = None
-                if disk_size == 15360 and capacity_disk_type == 'ssd':
-                    continue
+        for disk_size, disk_groups in get_raids_data()[key][slack_space].items():
+            for disk_group, disks_capacity in disk_groups.items():
+                host_disks_qty = int(str(disk_group)[0]) * int(str(disk_group)[1]) + int(str(disk_group)[0]) + 1 if capacity_disk_type == 'ssd' else 0
+                host_disks_hba_qty = int(str(disk_group)[0]) * int(str(disk_group)[1])
+                vsan_and_disks_limit = check_vsan_and_disks_limit(cpu_hosts, disks_capacity, host_disks_qty, server,
+                                                                  vsan_raw, vssd)
+                if vsan_and_disks_limit:
+                    if capacity_disk_type == 'ssd':
+                        hba = hba_adapter[8] if host_disks_hba_qty < 9 else hba_adapter[16]
+                    else:
+                        hba = None
+                    if disk_size == 15360 and capacity_disk_type == 'ssd':
+                        continue
 
-                vsan_disks_price = get_vsan_disks_price(capacity_disk_type=capacity_disk_type, disk_size=disk_size,
-                                                        disks_qty=disk_group, cache_disc=cache_disc,
-                                                        capacity_disks=capacity_disks)
-                host_price = (cpu['price'] * 2 + server['price'] + ram_1host * ram['price'] +
-                              esxi_disc[capacity_disk_type]['price'] + network_card['price'] + vsan_disks_price +
-                              (hba['price'] if hba else 0))
-                rms = math.ceil(host_price * currency / parameters['payback_period'] * parameters['coefficient'])
-                vmware = rms * cpu_hosts_n
-                works = math.ceil(
-                    get_works_price(hosts_qty=cpu_hosts_n, main=works_main, additional=works_add))
-                try:
-                    network_price = math.ceil(get_network_price(host_qty=cpu_hosts_n,
-                                                                network_card_qty=network_card_qty))
-                except TypeError:
-                    continue
-                total_price = vmware + works + network_price
-                # total_price = vmware + network_price
+                    vsan_disks_price = get_vsan_disks_price(capacity_disk_type=capacity_disk_type, disk_size=disk_size,
+                                                            disks_qty=disk_group, cache_disc=cache_disc,
+                                                            capacity_disks=capacity_disks)
+                    host_price = (cpu['price'] * 2 + server['price'] + ram_1host * ram['price'] +
+                                  esxi_disc[capacity_disk_type]['price'] + network_card['price'] + vsan_disks_price +
+                                  (hba['price'] if hba else 0))
+                    rms = math.ceil(host_price * currency / parameters['payback_period'] * parameters['coefficient'])
+                    vmware = rms * cpu_hosts_n
+                    works = math.ceil(
+                        get_works_price(hosts_qty=cpu_hosts_n, main=works_main, additional=works_add))
+                    try:
+                        network_price = math.ceil(get_network_price(host_qty=cpu_hosts_n,
+                                                                    network_card_qty=network_card_qty))
+                    except TypeError:
+                        continue
+                    total_price = vmware + works + network_price
+                    # total_price = vmware + network_price
 
-                # vcpu_available = math.ceil(
-                #     (cpu["cores_quantity"] * 2 * cpu_hosts * cpu_overcommit * parameters['max_cpu_usage']))
-                # vram_available = math.ceil(ram_1host * ram["ram_size"] * cpu_hosts * parameters['max_ram_usage'])
-                all_configs.append({
-                    f'Need hosts by CPU(n+{n})': cpu_hosts_n,
-                    'AllFlash vSAN': raid_config[key]['FTM'],
-                    'Failures to Tolerate': raid_config[key]['FTT'],
-                    'CPU overcommit': f'{cpu_overcommit}',
-                    'CPU': f'{cpu["name"]} {cpu["price"]}$ - 2 шт',
-                    'Server': f'{server["name"]} {server["price"]}$ - 1 шт',
-                    f'{ram["ram_size"]}Gb {ram["ram_gen"]} {ram["price"]}$' : f'{ram_1host} шт',
-                    'Esxi disk': f'{esxi_disc[capacity_disk_type]["disk_type"]} {esxi_disc[capacity_disk_type]["price"]}$ - 1 шт',
-                    'Cache disk': f'{cache_disc["capacity"]} {cache_disc["disk_type"]} {cache_disc["price"]}$ - {int(str(disk_group)[0])} шт',
-                    'Capacity disk': f'{disk_size} {capacity_disk_type} {capacity_disks[capacity_disk_type][disk_size]['price']}$ - {int(str(disk_group)[1]) * int(str(disk_group)[0])} шт',
-                    'Network card': f'{network_card["name"]} {network_card["price"]}$ - {network_card_qty} шт',
-                    'HBA adapter': f'{hba["name"]} - 1 шт' if hba else 0,
-                    'Admin main works': f'{works_main}',
-                    'Additional works': f'{works_add}',
-                    'Works price': f'{works} руб.',
-                    'Network': f'{network_price} руб.',
-                    # 'vCPU available': f'{vcpu_available}',
-                    # 'vRAM available': f'{vram_available}',
-                    # 'vSSD available': f'{disks_capacity * cpu_hosts}',
-                    # 'vSSD raw': f'{vsan_raw}',
-                    '1 host rms, Rub': f'{rms} руб.',
-                    'Total price, Rub': f'{total_price} руб.',
-                    'USD/RUB': f'{currency} руб.',
-                })
+                    # vcpu_available = math.ceil(
+                    #     (cpu["cores_quantity"] * 2 * cpu_hosts * cpu_overcommit * parameters['max_cpu_usage']))
+                    # vram_available = math.ceil(ram_1host * ram["ram_size"] * cpu_hosts * parameters['max_ram_usage'])
+                    all_configs.append({
+                        f'Need hosts by CPU(n+{n})': cpu_hosts_n,
+                        'AllFlash vSAN': raid_config[key]['FTM'],
+                        'Failures to Tolerate': raid_config[key]['FTT'],
+                        'CPU overcommit': f'{cpu_overcommit}',
+                        'CPU': f'{cpu["name"]} {cpu["price"]}$ - 2 шт',
+                        'Server': f'{server["name"]} {server["price"]}$ - 1 шт',
+                        f'{ram["ram_size"]}Gb {ram["ram_gen"]} {ram["price"]}$' : f'{ram_1host} шт',
+                        'Esxi disk': f'{esxi_disc[capacity_disk_type]["disk_type"]} {esxi_disc[capacity_disk_type]["price"]}$ - 1 шт',
+                        'Cache disk': f'{cache_disc["capacity"]} {cache_disc["disk_type"]} {cache_disc["price"]}$ - {int(str(disk_group)[0])} шт',
+                        'Capacity disk': f'{disk_size} {capacity_disk_type} {capacity_disks[capacity_disk_type][disk_size]['price']}$ - {int(str(disk_group)[1]) * int(str(disk_group)[0])} шт',
+                        'Network card': f'{network_card["name"]} {network_card["price"]}$ - {network_card_qty} шт',
+                        'HBA adapter': f'{hba["name"]} - 1 шт' if hba else 0,
+                        'Admin main works': f'{works_main}',
+                        'Additional works': f'{works_add}',
+                        'Works price': f'{works} руб.',
+                        'Network': f'{network_price} руб.',
+                        # 'vCPU available': f'{vcpu_available}',
+                        # 'vRAM available': f'{vram_available}',
+                        # 'vSSD available': f'{disks_capacity * cpu_hosts}',
+                        # 'vSSD raw': f'{vsan_raw}',
+                        '1 host rms, Rub': f'{rms} руб.',
+                        'Total price, Rub': f'{total_price} руб.',
+                        'USD/RUB': f'{currency} руб.',
+                    })
 
 
 def n_qty(cpu_hosts):
